9bb645bbbc54cf66f0a894cfcf3507f0bf09d42a
Jitender <jitender.96@yahoo.com>
2023-05-24 11:22:01 +0530

    intial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+document.getElementById("humburger").addEventListener("click", () => {
+  document.getElementById("open").classList.toggle("Open");
+  document.getElementById("close").classList.toggle("Close");
+  document.getElementById("links").classList.toggle("links-3");
+});
